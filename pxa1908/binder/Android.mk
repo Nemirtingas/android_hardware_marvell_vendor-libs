d94f10b8a1521f593825b534c655a159a0feea28
Nemirtingas <nanaki89@hotmail.fr>
2019-03-04 16:38:08 +0100

    Renamed library to vndbinder

--- pxa1908/binder/Android.mk
+++ pxa1908/binder/Android.mk
@@ -57,8 +57,8 @@ LOCAL_CFLAGS += -DUSE_MEMORY_HEAP_ION
 LOCAL_C_INCLUDES += hardware/samsung_slsi-cm/$(TARGET_BOARD_PLATFORM)/include
 endif
 
-LOCAL_MODULE := libbinder
-LOCAL_SHARED_LIBRARIES += liblog libcutils libutils
+LOCAL_MODULE := vndbinder
+LOCAL_SHARED_LIBRARIES += liblog libcutils libutils libmvmem
 
 LOCAL_CLANG := true
 LOCAL_SANITIZE := integer
@@ -78,20 +78,3 @@ LOCAL_SHARED_LIBRARIES += libion_exynos
 LOCAL_CFLAGS += -DUSE_MEMORY_HEAP_ION
 LOCAL_C_INCLUDES += hardware/samsung_slsi-cm/$(TARGET_BOARD_PLATFORM)/include
 endif
-
-LOCAL_MODULE := libbinder
-LOCAL_STATIC_LIBRARIES += libutils
-LOCAL_SRC_FILES := $(sources)
-ifeq ($(BOARD_NEEDS_MEMORYHEAPPMEM),true)
-LOCAL_C_INCLUDES += \
-    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_ADDITIONAL_DEPENDENCIES := \
-    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-endif
-ifneq ($(TARGET_USES_64_BIT_BINDER),true)
-ifneq ($(TARGET_IS_64_BIT),true)
-LOCAL_CFLAGS += -DBINDER_IPC_32BIT=1
-endif
-endif
-LOCAL_CFLAGS += -Werror
-include $(BUILD_STATIC_LIBRARY)
