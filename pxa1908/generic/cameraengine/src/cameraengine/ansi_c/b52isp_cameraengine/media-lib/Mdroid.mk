fb4a5d36f9fc3d4c9fda497fdbc402b94d0ddc0c
Nemirtingas <nanaki89@hotmail.fr>
2019-05-11 10:54:36 +0200

    Added libmedialib sources from xcover3lte (don't know if I will use it but... meh)

---
+++ pxa1908/generic/cameraengine/src/cameraengine/ansi_c/b52isp_cameraengine/media-lib/Mdroid.mk
@@ -0,0 +1,38 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+include $(LOCAL_PATH)/../../../../../CameraConfig.mk
+
+ifeq (${ENABLE_B52_ISP}, y)
+ifeq (${FAKE_B52_DRIVER}, y)
+LOCAL_SRC_FILES += \
+	src/MediaLib_fake.c \
+	src/v4l2subdev.c \
+	src/mediactl.c
+else
+LOCAL_SRC_FILES += \
+	src/MediaLib.c \
+	src/v4l2subdev.c \
+	src/mediactl.c
+endif
+
+LOCAL_CFLAGS += \
+    -I $(LOCAL_PATH)/../../_include \
+    -I bionic/libc/kernel/common/uapi \
+	-Werror
+
+LOCAL_CFLAGS += \
+	-I $(LOCAL_PATH)/src \
+	-D ANDROID
+
+LOCAL_SHARED_LIBRARIES := liblog libcutils
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_TARGET_ARCH := arm
+
+LOCAL_PRELINK_MODULE := false
+
+LOCAL_MODULE := libmedialib
+
+include $(BUILD_SHARED_LIBRARY)
+endif
